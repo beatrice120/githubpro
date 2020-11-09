6d67a37229ead71b1bbb4edf1225c070387109fb
beatrice120 <beatrice120>
2020-11-09 10:51:56 +0200

    adding backgroud

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -6,8 +6,8 @@ import { map } from 'rxjs/operators';
 export class ProfileService {
 
   private username: string;
-  private clientid = '203dd913322b3db7d987';
-  private clientsecret = '020c0b3665296b33d758edeeced30473ef44f7da';
+  private clientid = '';
+  private clientsecret = '';
 
 
   constructor(private http: Http) {
@@ -19,13 +19,13 @@ export class ProfileService {
     interface ApiResponse {
       login: string;
     }
-    // tslint:disable-next-line:max-line-length
+  
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
      .pipe(map(res => res.json()));
   }
 
   getProfileRepos() {
-    // tslint:disable-next-line:max-line-length
+   
     return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
       .pipe(map(res => res.json()));
   }
